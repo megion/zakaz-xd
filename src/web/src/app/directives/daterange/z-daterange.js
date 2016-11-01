076cdf109988c2eac68453fdf8313d82f31bda64
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2016-11-02 00:05:46 +0300

    daterange

---
+++ src/web/src/app/directives/daterange/z-daterange.js
@@ -0,0 +1,31 @@
+angular.module('zakaz-xd.directives.daterange', [
+    'ui.bootstrap'
+])
+    .directive('zDaterange', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                options: '=',
+                ngModel: '=',
+                required: '=',
+                name: '@'
+            },
+            templateUrl: 'app/directives/datepicker/z-datepicker.tpl.html',
+            controller: function ($scope) {
+                $scope.format = $scope.options.format || 'dd.MM.yyyy';
+
+                if ($scope.ngModel !== null && $scope.ngModel !== undefined) {
+                    if (typeof $scope.ngModel === 'string') {
+                        $scope.ngModel = new Date($scope.ngModel);
+                    }
+                }
+
+                $scope.open = function($event) {
+                    $event.preventDefault();
+                    $event.stopPropagation();
+                    $scope.opened = true;
+                };
+            }
+        };
+    })
+;
