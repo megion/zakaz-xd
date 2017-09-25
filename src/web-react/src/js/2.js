0696366aabdffcdc995b7a20fa56042854573d14
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2017-09-25 20:18:10 +0300

    test

--- src/web-react/src/js/2.js
+++ src/web-react/src/js/2.js
@@ -5,14 +5,28 @@ console.log(badLine);
 //$('#el1').off('.dropdown.data-api');
 $('#el1').on('click', function(e, options) {
     console.log('click:', e);
-    if(options && options.isClickDefaultEnable) {
-        return true;
+    if(options && options.enableDefaultClickEvent) {
+        console.log('Exit: enableDefaultClickEvent');
+        return;
     }
+
+    var parent = $('#el1').parent();
+    var isActive = parent.hasClass('open');
+    if(isActive) {
+        console.log('Exit: isActive');
+        return;
+    }
+
     e.stopPropagation();
     e.preventDefault();
+    if($('#el1').data("isPromiseRunnig")) {
+        return;
+    }
     
+    $('#el1').data("isPromiseRunnig", true);
     setTimeout(function() {
-        $('#el1').trigger( "click" , [ {isClickDefaultEnable: true} ]);
-    }, 5000);
+        $('#el1').data("isPromiseRunnig", false);
+        $('#el1').trigger( "click" , [ {enableDefaultClickEvent: true} ]);
+    }, 3000);
 });
 
